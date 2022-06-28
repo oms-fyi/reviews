927da0dca0a559b2ef6fe456f16d62ecdd076252
Matt Schlenker <m4ttschlenker@gmail.com>
2022-06-27 21:51:43 -0700

    Adds Google Analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+
+const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
+const Analytics = () => {
+  // Only really need to capture analytics in prod
+  if (!analyticsId) return null;
+
+  return (
+    <>
+      <Script
+        defer
+        src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){ dataLayer.push(arguments); }
+
+            gtag("js", new Date());
+            gtag("config", "${analyticsId}");
+          `}
+      </Script>
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
