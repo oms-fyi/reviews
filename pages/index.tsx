ad88e686319d3109b37b2c7c602f1794a507c847
Matt Schlenker <m4ttschlenker@gmail.com>
2022-07-14 21:19:14 -0700

    Filters working, but can't see courses with 0 reviews every becauses derived fields are NaN.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import { useMemo, useState } from "react";
+import { Dispatch, Fragment, SetStateAction, useState } from "react";
+
 import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+import { Input } from "../components/Input";
 import { SortIcon } from "../components/SortIcon";
-import { Filters, CourseFilter } from "../components/Filters";
+
 import type { Course } from "../@types";
 import { getCourses } from "../lib/sanity";
 
@@ -28,24 +33,49 @@ interface SortConfig {
   direction: "desc" | "asc";
 }
 
+function getDefaultInputValue(value: number | undefined): string {
+  if (typeof value === "undefined" || isNaN(value)) {
+    return "";
+  } else {
+    return value.toString();
+  }
+}
+
 const Home: NextPage<HomePageProps> = ({ courses }) => {
   const [sort, setSort] = useState<SortConfig>({
     attribute: "reviewCount",
     direction: "desc",
   });
 
-  const [filter, setFilter] = useState<CourseFilter>();
+  // By default only show courses with 1+ review
+  const [minReviewCount, setMinReviewCount] = useState<number>(1);
+  const [maxReviewCount, setMaxReviewCount] = useState<number>();
 
-  const filtered = useMemo(
-    () => courses.filter((course) => filter?.(course) || true),
-    [courses, filter]
-  );
+  const [minRating, setMinRating] = useState<number>();
+  const [maxRating, setMaxRating] = useState<number>();
 
-  const sorted = useMemo(() => {
-    const { attribute } = sort;
+  const [minDifficulty, setMinDifficulty] = useState<number>();
+  const [maxDifficulty, setMaxDifficulty] = useState<number>();
 
-    return filtered.sort((a, b) => {
+  const [minWorkload, setMinWorkload] = useState<number>();
+  const [maxWorkload, setMaxWorkload] = useState<number>();
+
+  const current = courses
+    .filter(({ reviewCount, rating, difficulty, workload }) => {
+      function between(value: number, min: number, max: number): boolean {
+        return value >= min && value <= max;
+      }
+
+      return (
+        between(reviewCount, 0, maxReviewCount || Infinity) &&
+        between(rating, minRating || 1, maxRating || 5) &&
+        between(difficulty, minDifficulty || 1, maxDifficulty || 5) &&
+        between(workload, minWorkload || 1, maxWorkload || 100)
+      );
+    })
+    .sort((a, b) => {
       const ordering = sort.direction === "asc" ? 1 : -1;
+      const { attribute } = sort;
 
       if (attribute === "name") {
         return a[attribute].localeCompare(b[attribute]) * ordering;
@@ -57,7 +87,6 @@ const Home: NextPage<HomePageProps> = ({ courses }) => {
         return ((a[attribute] as number) - (b[attribute] as number)) * ordering;
       }
     });
-  }, [sort, filtered]);
 
   function toggleSort(attribute: SortConfig["attribute"]) {
     if (sort.attribute !== attribute) {
@@ -106,12 +135,198 @@ const Home: NextPage<HomePageProps> = ({ courses }) => {
                   </button>
                 </div>
               </div>
-              <div className="text-right">
-                <Filters
-                  onChange={(nextFilter: CourseFilter) => setFilter(nextFilter)}
-                />
+              <div className="flex justify-end">
+                <div>
+                  <Popover className="relative">
+                    {({ open }) => (
+                      <>
+                        <Popover.Button
+                          className={`
+                ${open ? "" : "text-opacity-90"}
+                w-24 group inline-flex items-center rounded-md bg-indigo-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                        >
+                          <span>{open ? "Done" : "Filters"}</span>
+                          <ChevronDownIcon
+                            className={`${open ? "" : "text-opacity-70"}
+                  ml-2 h-5 w-5 text-indigo-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
+                            aria-hidden="true"
+                          />
+                        </Popover.Button>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0 translate-y-1"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-1"
+                        >
+                          <Popover.Panel className="absolute right-0 z-10 mt-3 px-4 sm:px-0">
+                            <article className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                              <form className="bg-white p-7">
+                                <div className="mb-6">
+                                  <p className="mb-4 text-xs text-gray-500 uppercase">
+                                    Filter by review count
+                                  </p>
+                                  <fieldset className="flex gap-2">
+                                    <legend className="sr-only">
+                                      Review Count
+                                    </legend>
+                                    <Input
+                                      id="minReview"
+                                      type="text"
+                                      label="Min Reviews"
+                                      placeholder="1"
+                                      defaultValue={getDefaultInputValue(
+                                        minReviewCount
+                                      )}
+                                      inputMode="decimal"
+                                      onBlur={(e) => {
+                                        console.log("blur", e);
+                                        setMinReviewCount(
+                                          parseFloat(e.currentTarget.value)
+                                        );
+                                      }}
+                                    />
+                                    <Input
+                                      id="maxReview"
+                                      type="text"
+                                      label="Max Reviews"
+                                      placeholder="100"
+                                      defaultValue={getDefaultInputValue(
+                                        maxReviewCount
+                                      )}
+                                      inputMode="decimal"
+                                      onBlur={(e) => {
+                                        setMaxReviewCount(
+                                          parseFloat(e.currentTarget.value)
+                                        );
+                                      }}
+                                    />
+                                  </fieldset>
+                                </div>
+                                <div className="mb-6">
+                                  <p className="mb-4 text-xs text-gray-500 uppercase">
+                                    Filter by stats
+                                  </p>
+                                  <div className="flex flex-col gap-6">
+                                    <fieldset className="flex gap-2">
+                                      <legend className="sr-only">
+                                        Rating
+                                      </legend>
+                                      <Input
+                                        id="minRating"
+                                        type="text"
+                                        label="Min Rating"
+                                        placeholder="1"
+                                        defaultValue={getDefaultInputValue(
+                                          minRating
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMinRating(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                      <Input
+                                        id="maxRating"
+                                        type="text"
+                                        label="Max Rating"
+                                        placeholder="5"
+                                        defaultValue={getDefaultInputValue(
+                                          maxRating
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMaxRating(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                    </fieldset>
+                                    <fieldset className="flex gap-2">
+                                      <legend className="sr-only">
+                                        Difficulty
+                                      </legend>
+                                      <Input
+                                        id="minDifficulty"
+                                        type="text"
+                                        label="Min Difficulty"
+                                        placeholder="1"
+                                        defaultValue={getDefaultInputValue(
+                                          minDifficulty
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMinDifficulty(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                      <Input
+                                        id="maxDifficulty"
+                                        type="number"
+                                        label="Max Difficulty"
+                                        placeholder="5"
+                                        defaultValue={getDefaultInputValue(
+                                          maxDifficulty
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMaxDifficulty(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                    </fieldset>
+                                    <fieldset className="flex gap-2">
+                                      <legend className="sr-only">
+                                        Workload
+                                      </legend>
+                                      <Input
+                                        id="minWorkload"
+                                        type="number"
+                                        label="Min Workload"
+                                        placeholder="10"
+                                        defaultValue={getDefaultInputValue(
+                                          minWorkload
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMinWorkload(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                      <Input
+                                        id="maxWorkload"
+                                        type="number"
+                                        label="Max Workload"
+                                        placeholder="20"
+                                        defaultValue={getDefaultInputValue(
+                                          maxWorkload
+                                        )}
+                                        inputMode="decimal"
+                                        onBlur={(e) => {
+                                          setMaxWorkload(
+                                            parseFloat(e.currentTarget.value)
+                                          );
+                                        }}
+                                      />
+                                    </fieldset>
+                                  </div>
+                                </div>
+                              </form>
+                            </article>
+                          </Popover.Panel>
+                        </Transition>
+                      </>
+                    )}
+                  </Popover>
+                </div>
               </div>
-              <div className="inline-block min-w-full py-3 align-middle">
+              <div className="inline-block min-w-full py-4 align-middle">
                 <div className="shadow-sm ring-1 ring-black ring-opacity-5">
                   <table
                     className="min-w-full border-separate"
@@ -202,7 +417,7 @@ const Home: NextPage<HomePageProps> = ({ courses }) => {
                       </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200 bg-white">
-                      {sorted.map(
+                      {current.map(
                         (
                           {
                             id,
