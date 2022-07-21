625a0fe58cd0c94543f1546be439a4611a5bb20a
Matt Schlenker <m4ttschlenker@gmail.com>
2022-07-20 18:07:50 -0700

    Update env variables to make sanity/vercel integration env var names.

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,7 +1,7 @@
 import clientFactory from "@sanity/client";
 
-const projectId = process.env.SANITY_PROJECT_ID;
-const dataset = process.env.SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 if (!projectId || !dataset) {
   throw new Error("Sanity config not found!");
