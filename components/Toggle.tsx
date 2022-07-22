117830f0948a2ec1862b8404d62367e214835ecd
Matt Schlenker <m4ttschlenker@gmail.com>
2022-07-21 22:57:16 -0700

    Remove tailwind classnames utility and add npm package.

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,9 +1,6 @@
-import { useState, FC } from "react";
+import { FC } from "react";
 import { Switch } from "@headlessui/react";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import classNames from "classnames";
 
 interface ToggleProps {
   enabled: boolean;
@@ -18,14 +15,14 @@ export const Toggle: FC<ToggleProps> = ({ enabled, onChange, label }) => {
         checked={enabled}
         onChange={onChange}
         className={classNames(
-          enabled ? "bg-indigo-600" : "bg-gray-200",
+          { "bg-indigo-600": enabled, "bg-gray-200": !enabled },
           "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         )}
       >
         <span
           aria-hidden="true"
           className={classNames(
-            enabled ? "translate-x-5" : "translate-x-0",
+            { "translate-x-5": enabled, "translate-x-0": !enabled },
             "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
           )}
         />
