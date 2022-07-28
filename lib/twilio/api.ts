02be430ec757602b39b247beea08397c5392fa48
Matt Schlenker <m4ttschlenker@gmail.com>
2022-07-28 13:05:16 -0700

    No need to return anything from code generating function.

--- lib/twilio/api.ts
+++ lib/twilio/api.ts
@@ -1,17 +1,12 @@
-import { VerificationInstance } from "twilio/lib/rest/verify/v2/service/verification";
 import { twilioClient } from "./client";
 
 const VERIFY_SERVICE_SID = "VA8fe01f6ccca6c198aaaf592da51ad99c";
 
-export async function sendCodeToUser(
-  username: string
-): Promise<VerificationInstance> {
+export async function sendCodeToUser(username: string): Promise<void> {
   const email = `${username}@gatech.edu`;
 
   // https://www.twilio.com/docs/verify/email
-  const verification = await twilioClient.verify.v2
+  await twilioClient.verify.v2
     .services(VERIFY_SERVICE_SID)
     .verifications.create({ to: email, channel: "email" });
-
-  return verification;
 }
