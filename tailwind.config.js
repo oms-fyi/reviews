89a1d03fc241bf276e85a14e49e992145746b014
Matt Schlenker <m4ttschlenker@gmail.com>
2022-08-13 09:43:44 -0700

    Move everything under src.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ const forms = require('@tailwindcss/forms');
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
