58f0588c977eb3204380d82cec7011e52e032a35
Matt Schlenker <m4ttschlenker@gmail.com>
2022-08-25 15:29:31 -0700

    Adds banner in for announcement.

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,15 +4,8 @@ export function Banner(): JSX.Element {
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="text-center ">
           <p className="font-medium text-white">
-            <span className="inline">The reddit AMA is live!</span>
-            <span className="ml-2 inline-block">
-              <a
-                href="https://www.reddit.com/r/OMSCS/comments/wnf87i/hey_omscs_i_am_the_new/"
-                className="text-white font-bold border-b"
-              >
-                {" "}
-                Ask me anything <span aria-hidden="true">&rarr;</span>
-              </a>
+            <span>
+              Course pages now have syllabi, textbooks, descriptions and more!
             </span>
           </p>
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import Script from "next/script";
 import "src/styles/globals.css";
 import { Header } from "src/components/header";
 import { Footer } from "src/components/footer";
+import { Banner } from "src/components/banner";
 
 const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Banner />
       <Header />
       <div className="grow">
         <Component {...pageProps} />
