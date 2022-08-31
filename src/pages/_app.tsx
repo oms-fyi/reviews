638c5e9434c4e521c2cc0aa082f1b357c502f5e5
Matt Schlenker <m4ttschlenker@gmail.com>
2022-08-31 06:19:41 -0700

    Drops banner.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,14 @@ import Script from "next/script";
 import "src/styles/globals.css";
 import { Header } from "src/components/header";
 import { Footer } from "src/components/footer";
-import { Banner } from "src/components/banner";
+// import { Banner } from "src/components/banner";
 
 const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Banner />
+      {/* <Banner /> */}
       <Header />
       <div className="grow">
         <Component {...pageProps} />
