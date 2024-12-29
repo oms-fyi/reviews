6f2553af6af1d8a5c9de116495d452e0703d164e
Matthew Schlenker <m4ttschlenker@gmail.com>
2024-12-29 13:50:51 -0800

    Remove footer component and use BMAC embed directly.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 
-import { Footer } from "src/components/footer";
 import { Header } from "src/components/header";
 import "src/styles/globals.css";
 
@@ -17,7 +16,24 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <Component {...pageProps} />
         <SpeedInsights />
       </div>
-      <Footer />
+      <footer className="bg-white">
+        <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            <a href="https://www.buymeacoffee.com/omstech">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=omstech&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+                alt="Donate to omscentral.com"
+              />
+            </a>
+          </div>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-base text-gray-400">
+              &copy; {new Date().getFullYear()} OMSCentral. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
       <Analytics />
     </>
   );
