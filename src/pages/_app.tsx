5f244ff84f7428d3725a5e67e5873e007889e9ea
Matthew Schlenker <m4ttschlenker@gmail.com>
2024-12-29 13:40:46 -0800

    Font cleanup.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
 import { Footer } from "src/components/footer";
 import { Header } from "src/components/header";
 import "src/styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Header />
-      <div className="grow">
+      <div className={`${inter.className} grow`}>
         <Component {...pageProps} />
         <SpeedInsights />
       </div>
